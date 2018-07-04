7aa0d988ac9ddc592113a283c66a2171c633c2d0
Hiroya Hinomori <>
2018-07-04 11:00:28 +0900

    新規プロジェクト作成

---
+++ RxAVPlayer/RxAVPlayer.h
@@ -0,0 +1,19 @@
+//
+//  RxAVPlayer.h
+//  RxAVPlayer
+//
+//  Created by HINOMORI HIROYA on 2018/07/04.
+//  Copyright © 2018年 HINOMORI HIROYA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxAVPlayer.
+FOUNDATION_EXPORT double RxAVPlayerVersionNumber;
+
+//! Project version string for RxAVPlayer.
+FOUNDATION_EXPORT const unsigned char RxAVPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxAVPlayer/PublicHeader.h>
+
+
---
+++ RxAVPlayer/RxAVPlayer.swift
@@ -0,0 +1,21 @@
+//
+//  RxAVPlayer.swift
+//  RxAVPlayer
+//
+//  Created by HINOMORI HIROYA on 2018/07/04.
+//  Copyright © 2018年 HINOMORI HIROYA. All rights reserved.
+//
+
+import UIKit
+
+class RxAVPlayer: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
