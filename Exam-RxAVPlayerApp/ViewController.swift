b2733e8a892528ae5f9259e87ac8ef2e4c4792c7
Hiroya Hinomori <>
2018-07-05 22:37:50 +0900

    時間表示をコントロールできるように修正

--- Exam-RxAVPlayerApp/ViewController.swift
+++ Exam-RxAVPlayerApp/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         player.autoplay = true
         player.visibleSkipSeconds = 2.0
+        player.dateFormatString = "m:ss"
         player.mute = true
         player.url = URL(string: "https://s3.us-east-2.amazonaws.com/vjs-nuevo/hls/m3u8/playlist.m3u8")
     }
